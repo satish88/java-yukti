6513cf2ad3683a18d24bcd83017a0797e3275d88
Yukti Aggarwal <yukti.1397@gmail.com>
2021-02-08 23:10:43 +0530

    Write 2 methods
    Signed-off-by: Yukti13 <Yukti.1397@gmail.com>

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,13 @@
+public class FirstClass {
+
+    public static int scoring(int x, int y) {
+        return x - y;
+    }
+
+
+    public static String introducing(String a, String b) {
+        return a + b;
+    }
+
+
+}
