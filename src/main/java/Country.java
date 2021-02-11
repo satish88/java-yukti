05b5bb73c762a51cda7df029cc9ea33d25634874
Yukti Aggarwal <yukti.1397@gmail.com>
2021-02-11 23:03:34 +0530

    Write a programme That gives information of the country.
    Signed-off-by: Yukti13 <Yukti.1397@gmail.com>

--- src/main/java/Country.java
+++ src/main/java/Country.java
@@ -11,9 +11,29 @@ public class Country {
             continent = "In Asia,";
             capital = " beijing is the capital,";
             areaAndPlaceInWorld = " area is 9.597 million km² and 4th biggest country,";
-            currency = " Renminbi is the currency,";
+            currency = " Chinese Yuan,";
             president = " Xi Jinping is the president, ";
-            populationAndRank = " and population is 139.77 crores and has 1st rank in population in whole world";
+            populationAndRank = " and population is 139.77 crores and has 1st rank in whole world";
+            return continent + capital + areaAndPlaceInWorld + currency + president + populationAndRank;
+        }
+
+        if (country.equals("India")) {
+            continent = "In Aisa";
+            capital = " New delhi is the capital, ";
+            areaAndPlaceInWorld = " area is 3.287 million km² and 7th biggest country, ";
+            currency = " Indian Rupee, ";
+            president = " Ram Nath Kovind is the president, ";
+            populationAndRank = " and population is 136.64 crores and has 2nd rank in the whole world";
+            return continent + capital + areaAndPlaceInWorld + currency + president + populationAndRank;
+        }
+
+        if (country.equals("USA")) {
+            continent = "In North America";
+            capital = " Washington, D.C. is the capital, ";
+            areaAndPlaceInWorld = " area is 9.834 million km² and 3rd biggest country, ";
+            currency = " U.S. dollar, ";
+            president = " Joe Biden is the president, ";
+            populationAndRank = " and population is 32.82 crores and has 3rs rank in the whole world";
             return continent + capital + areaAndPlaceInWorld + currency + president + populationAndRank;
         } else {
             return "No information available or check the country name.";
