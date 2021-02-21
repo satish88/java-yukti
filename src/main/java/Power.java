312c001824dc24ac34ee7a1d10f9b5339b5ee23a
Yukti13 <Yukti.1397@gmail.com>
2021-02-21 11:04:28 +0530

    power loop
    Signed-off-by: Yukti13 <Yukti.1397@gmail.com>

--- src/main/java/Power.java
+++ src/main/java/Power.java
@@ -1,10 +1,43 @@
 public class Power {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         int p = 123;
         int power = 5;
-        for (int i = 1; i <= p; i = i * 5) {
+        for (int i = 5; i <= p; i = i * 5) {
             power = power * i;
             System.out.println(power);
         }
     }
+
+    public static void main(String[] args) {
+        int i = 3;
+        int power = 4;
+        int result = i;
+
+        // 2 * 2 * 2
+        // 5^4 = 5 * 5 * 5*5
+
+        // counter = 1 -> power
+        //       i * i;
+
+        for (int counter = 1; counter < power; counter = counter + 1) {
+            result = result * i;
+        }
+        System.out.println(result);
+
+    }
+
+    // i = 2, power = 3
+    // counter = 1
+    // 1 <=3  true
+    // i = 2
+    // counter = 2
+    // 2 <= 3 true
+    // i = 4
+    // counter = 3
+    // 3 <= 3 true
+    // i = 8
+    // counter = 4
+    // 4 <= 3 false
+    // Result= 8
+
 }
