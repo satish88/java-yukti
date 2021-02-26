193f1310e592ff29225eda002600846d357fd70d
Yukti Aggarwal <yukti.1397@gmail.com>
2021-02-26 18:14:56 +0530

    JY-3 bug issue is fixed.
    Signed-off-by: Yukti13 <Yukti.1397@gmail.com>

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,9 +3,16 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("hello");
 
-        Factorial programme = new Factorial();
+        Factorial result = new Factorial();
         int factorial = Factorial.calculateFactorial(4);
         System.out.println(factorial);
+
+        Sum calculation = new Sum();
+        int addition = Sum.sumWithOneInput(10);
+        System.out.println(addition);
+
+        addition = Sum.sumWithTwoInput(-5, 3);
+        System.out.println(addition);
 
     }
 }
--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,7 +1,6 @@
 public class Sum {
 
-    //
-    public static void sumWithOneInput(int n) {
+    public static int sumWithOneInput(int n) {
         int result = 0;
         if (n > 0) {
             for (int number = 1; number <= n; number = number + 1) {
@@ -12,32 +11,10 @@ public class Sum {
                 result = result + number;
             }
         }
-        System.out.println(result);
-
-        //result = 0
-        // number = 1
-        // 1<=5 true
-        //result = 1
-        // number = 2
-        // 2<=5 true
-        // result = 3
-        // number = 3
-        //3<=5 true
-        // result 6
-        // number 4
-        // 4 < = 5 true
-        //result 10
-        // number 5
-        // 5<=5 true
-        // result 15
-        // number 6
-        // 6<= 5 false
-        // print result 15
-
-
+        return result;
     }
 
-    public static void sumWithTwoInput(int a, int n) {
+    public static int sumWithTwoInput(int a, int n) {
         int result = 0;
         if (a < n) {
             for (int number = a; number <= n; number = number + 1) {
@@ -48,27 +25,6 @@ public class Sum {
                 result = result + number;
             }
         }
-        System.out.println(result);
-
-        //result 0
-        // number 2
-        // 2<= 4 true
-        //result 2
-        //number 3
-        //3<=4 true
-        // result 5
-        // number 4
-        // 4<=4 true
-        //result 9
-        //number 5
-        // 5<= 4 false
-        // print result 9
-
+        return result;
     }
-
-    public static void main(String[] args) {
-        sumWithOneInput(-3);
-        sumWithTwoInput(-3, -3);
-    }
-
 }
