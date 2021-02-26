86f983af25f255f16669ec1e217b05f67405a7c5
Yukti Aggarwal <yukti.1397@gmail.com>
2021-02-26 18:14:56 +0530

    JY-2 bug issue is fixed.
    Signed-off-by: Yukti13 <Yukti.1397@gmail.com>

--- src/main/test/SumTest.java
+++ src/main/test/SumTest.java
@@ -1,26 +1,26 @@
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class SumTest {
     @Test
     public static void testForCalculatingSumFromOneToNNumber() {
         Sum sum = new Sum();
-        int result = sum.sumWithOneInput(3);
+        int result = Sum.sumWithOneInput(3);
         assertEquals(result,6, "sum of 1 to 3 numbers" );
 
     }
     @Test
     public static void testForCalculatingSumOfRandomSeriesOfForwardingNNumber() {
         Sum sum = new Sum();
-        int result = sum.sumWithTwoInput(3,5);
+        int result = Sum.sumWithTwoInput(3, 5);
         assertEquals(result,12, "sum of 3 to 5 numbers" );
 
     }
     @Test
     public static void testForCalculatingSumOfBackwardSeriesOfNumber() {
         Sum sum = new Sum();
-        int result = sum.sumWithTwoInput(3,-1);
+        int result = Sum.sumWithTwoInput(3, -1);
         assertEquals(result,5, "sum of 3 to -1 numbers" );
 
     }
