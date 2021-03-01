f7988ff4f066a2b3e63fc8f3fff04b335d8641da
Yukti13 <Yukti.1397@gmail.com>
2021-03-01 23:01:33 +0530

    edit

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,5 +1,6 @@
 public class Sum {
 
+    // problem 4
     //Write a program that asks the user for a number n
     // and prints the sum of the numbers 1 to n
 
--- src/main/java/SumOrProduct.java
+++ src/main/java/SumOrProduct.java
@@ -1,5 +1,6 @@
 public class SumOrProduct {
 
+    //problem 6
     //Write a program that asks the user for a
     // number n and gives them the possibility to choose
     // between computing the sum and computing the product of 1,…,n.
