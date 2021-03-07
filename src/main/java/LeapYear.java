db1cc318feb3b37053bddd8d0b28123bf3bac9d3
Yukti13 <Yukti.1397@gmail.com>
2021-03-07 23:45:44 +0530

    Revert "JY-15 Leap year method changed in List type."
    This reverts commit 8fb389b5

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,27 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class LeapYear {
 
     //problem 10
     //Write a program that prints the next 20 leap years.
-    public static boolean checkLeapYear(int year) {
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else return year % 4 == 0;
-    }
-
-    public static List<Integer> printLeapYear(int year, int numbersOfYear) {
-        int n = numbersOfYear;
-        List<Integer> leapYear = new ArrayList();
-        for (int a = 0; a <= (n * 4); a++) {
-            year = year + 1;
-            if (checkLeapYear(year)) {
-                leapYear.add(year);
+    public static int calculateLeapYear(int year) {
+        int leapYear = 1;
+        if (year % 4 == 0) {
+            for (int a = 0; a < 80; a = a + 4) {
+                leapYear = year + a;
+                System.out.println(leapYear);
             }
+            return leapYear + 4;
+        } else if ((year - 1) % 4 == 0) {
+            int leapYearPlusOneExtraMoreYear = year + 3;
+            for (int a = 0; a < 80; a = a + 4) {
+                leapYear = leapYearPlusOneExtraMoreYear + a;
+                System.out.println(leapYear);
+            }
+            return leapYear + 4;
+        } else if ((year - 2) % 4 == 0) {
+            int leapYearPlusTwoExtraMoreYear = year + 2;
+            for (int a = 0; a < 80; a = a + 4) {
+                leapYear = leapYearPlusTwoExtraMoreYear + a;
+                System.out.println(leapYear);
+            }
+            return leapYear + 4;
+        } else if ((year - 3) % 4 == 0) {
+            int leapYearPlusThreeExtraMoreYear = year + 1;
+            for (int a = 0; a < 80; a = a + 4) {
+                leapYear = leapYearPlusThreeExtraMoreYear + a;
+                System.out.println(leapYear);
+            }
+            return leapYear + 4;
+        } else {
+            return 0;
         }
-        return leapYear;
     }
 }
