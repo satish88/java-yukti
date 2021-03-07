5d3d1ed6c2080a0d69d78988aa4eef7c84483d89
Yukti13 <Yukti.1397@gmail.com>
2021-03-07 23:51:26 +0530

    move test in test folder.

--- src/main/test/SumOfDivisibleNumberThreeOrFiveTest.java
+++ src/main/test/SumOfDivisibleNumberThreeOrFiveTest.java
@@ -1,18 +1,18 @@
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 @Test
 public class SumOfDivisibleNumberThreeOrFiveTest {
 
     public void testForPositiveNumberSeries(){
         SumOfDivisibleNumberThreeOrFive sum = new SumOfDivisibleNumberThreeOrFive();
-        int result = sum. solutionOfProblemFive(5,10);
+        int result = SumOfDivisibleNumberThreeOrFive.solutionOfProblemFive(5, 10);
         assertEquals(result, 30, "sum of divisible of 3 & 5 in number series 5 to 10");
     }
 
     public void testForNegativeNumberSeries(){
         SumOfDivisibleNumberThreeOrFive sum = new SumOfDivisibleNumberThreeOrFive();
-        int result = sum. solutionOfProblemFive(1,-5);
+        int result = SumOfDivisibleNumberThreeOrFive.solutionOfProblemFive(1, -5);
         assertEquals(result, -8, "sum of divisible of 3 & 5 in number series 1 to -5");
 
 
